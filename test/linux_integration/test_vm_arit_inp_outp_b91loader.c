7d21ae3a6c3924ef7782b7a18739ff33917b4617
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-03-24 10:47:03 +0200

    Migrated linux integration test to Unix Unit

--- test/linux_integration/test_vm_arit_inp_outp_b91loader.c
+++ test/linux_integration/test_vm_arit_inp_outp_b91loader.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include "punit.h"
+#include "unixunit.h"
 #include "config.h"
 #include "vm.h"
 
@@ -13,30 +14,17 @@
 uint32_t mem[1024];
 int memsize;
 
-/* Stdin writer */
-FILE * stdin_writer;
-int my_pipe[2];
-
-static void open_stdin_writer();
-static void write_stdin(char * str);
-
 static void setup()
 {
     memsize = sizeof(mem) / sizeof(uint32_t);
     memset(mem, 0x0, memsize);
 
-    /* Open pipe */
-    if (pipe(my_pipe) == -1) {
-        fprintf(stderr, "Pipe failed.");
-        exit(1);
-    }
+    uu_open_pipe();
 }
 
 static void teardown()
 {
-    /* Close pipe */
-    close(my_pipe[0]);
-    close(my_pipe[1]);
+    uu_close_pipe();
 }
 
 static char * test_pow()
@@ -47,10 +35,10 @@ static char * test_pow()
     char * input[] = {"2\n", "4\n"};
 
     /* Write input values to stdin */
-    open_stdin_writer();
-    write_stdin(input[0]);
-    write_stdin(input[1]);
-    fclose(stdin_writer);
+    uu_open_stdin_writer();
+    uu_write_stdin(input[0]);
+    uu_write_stdin(input[1]);
+    uu_close_stdin_writer();
 
     b91_loader_read_file(mem, memsize, &code_size, "asm/pow.b91");
     pu_assert("Error while loading a b91 binary file.", err == 0);
@@ -58,7 +46,7 @@ static char * test_pow()
     vm_init_state(&state, code_size, memsize);
     vm_run(&state, mem);
 
-    pu_assert("Result of 2^4 == 16", state.regs[1] == 16);
+    pu_assert_equal("Result of 2^4 == 16", state.regs[1], 16);
     return 0;
 }
 
@@ -70,9 +58,9 @@ static char * test_arrinit()
     char input[] = "3\n";
 
     /* Write input values to stdin */
-    open_stdin_writer();
-    write_stdin(input);
-    fclose(stdin_writer);
+    uu_open_stdin_writer();
+    uu_write_stdin(input);
+    uu_close_stdin_writer();
 
     b91_loader_read_file(mem, memsize, &code_size, "asm/arrinit.b91");
     pu_assert("Error while loading a b91 binary file.", err == 0);
@@ -91,22 +79,11 @@ static char * test_arrinit()
 
 static void all_tests()
 {
-    pu_run_test(test_pow);
-    pu_run_test(test_arrinit);
+    pu_def_test(test_pow, PU_RUN);
+    pu_def_test(test_arrinit, PU_RUN);
 }
 
 int main(int argc, char **argv)
 {
     return pu_run_tests(&all_tests);
-}
-
-static void open_stdin_writer()
-{
-    dup2(my_pipe[0], STDIN_FILENO);
-    stdin_writer = fdopen(my_pipe[1], "w");
-}
-
-static void write_stdin(char * str)
-{
-    fwrite(str, 1, strlen(str), stdin_writer);
 }
